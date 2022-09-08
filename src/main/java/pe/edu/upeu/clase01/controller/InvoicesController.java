111dc38ec741c5c547b639576725c4f986e29004
Noodlexz <Noodlexz@DESKTOP-4JHBMU6>
2022-09-08 08:36:39 -0500

    a

--- src/main/java/pe/edu/upeu/clase01/controller/InvoicesController.java
+++ src/main/java/pe/edu/upeu/clase01/controller/InvoicesController.java
@@ -7,16 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import pe.edu.upeu.clase01.entity.Invoices;
+import pe.edu.upeu.clase01.entity.Suppliers;
 import pe.edu.upeu.clase01.service.InvoicesService;
+import pe.edu.upeu.clase01.service.SuppliersService;
 
 @RestController
 public class InvoicesController {
 
 	@Autowired
 	private InvoicesService invoicesService;
+
 	
 	@GetMapping("/invoices")
 	public List<Invoices> getList(){
 		return invoicesService.readAll();
 	}
+	
 }
